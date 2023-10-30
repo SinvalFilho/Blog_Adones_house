d11492a663dbc4c55bc21276a442880e35b87b7b
SinvalFilho <sinvalleitefilho@gmail.com>
2023-10-29 23:01:23 -0300

    teste 4

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,21 +1,72 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import User from 'App/Models/User'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UsersController {
-  public async index() {
-    const users = await User.all()
-    return users
+  public async index({ response }: HttpContextContract) {
+    try {
+      const users = await User.all()
+      return response.status(200).json(users)
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao Buscar usuários' })
+    }
   }
-  public async store() {
-    const user = await User.create({
-      name: 'Sinval',
-      email: 'sinvalfilho@gmail.com',
-      password: '1234',
-    })
-    return user
+  public async store({ request, response }: HttpContextContract) {
+    try {
+      const userData = request.only(['name', 'email', 'password'])
+      const user = await User.create(userData)
+      return response.status(201).json(user)
+    } catch (error) {
+      return response.status(400).json({ message: 'Erro ao criar usuário' })
+    }
   }
-  public async show() {}
-  public async update() {}
-  public async destroy() {}
+  public async show({ params, response }: HttpContextContract) {
+    try {
+      const userId = params.id
+      const user = await User.find(userId)
+
+      if (!user) {
+        return response.status(404).json({ message: 'Usuário não encontrado' })
+      }
+
+      return response.status(200).json(user)
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao buscar usuário' })
+    }
+  }
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const userId = params.id
+      const user = await User.find(userId)
+
+      if (!user) {
+        return response.status(404).json({ message: 'Usuário não encontrado' })
+      }
+
+      const userData = request.only(['name', 'email', 'password'])
+      user.merge(userData)
+      await user.save()
+
+      return response.status(200).json(user)
+    } catch (error) {
+      return response.status(400).json({ message: 'Erro ao atualizar usuário' })
+    }
+  }
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      const userId = params.id
+      const user = await User.find(userId)
+
+      if (!user) {
+        return response.status(404).json({ message: 'Usuário não encontrado' })
+      }
+
+      await user.delete()
+
+      return response.status(200).json({ message: 'Usuário excluido com sucesso' })
+    } catch (error) {
+      return response.status(400).json({ message: 'Erro ao atualizar usuário' })
+    }
+  }
 }
--- contracts/auth.ts
+++ contracts/auth.ts
@@ -5,7 +5,7 @@
  * file.
  */
 
-import User from 'App/Models/User'
+import User from 'App/Models/Post.1'
 
 declare module '@ioc:Adonis/Addons/Auth' {
   /*
