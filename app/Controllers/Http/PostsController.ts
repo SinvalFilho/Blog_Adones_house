32c1ca1db096601893e2555de476a19c900e23dc
SinvalFilho <sinvalleitefilho@gmail.com>
2023-11-16 16:42:02 -0300

    modificacao 7

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,3 +1,10 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class PostsController {}
+export default class PostsController {
+    public async store({ auth, request, response }: HttpContextContract) {
+        const { title, description } = request.only(['title', 'description'])
+        await User = await auth.authenticate()
+        await User.related('posts').create({ title, description })
+        return response.status(204)
+    }
+}
